1aea30573e23bfec402bf3fa33aaadd1df3e3e94
dodi-wan <dodsenergi@gmail.com>
2025-04-16 08:55:37 +0700

    added file bottom

---
+++ bottom.java
@@ -0,0 +1,19 @@
+
+class jeans {
+    String  Long,
+            Short;
+}
+
+public class bottom {
+
+    public static void main(String [] args) {
+
+        jeans Jeans = new jeans();
+        Jeans.Long = "regular";
+        Jeans.Short = "relax fit";
+
+        System.out.println(Jeans.Long + "\n" + Jeans.Short);
+
+    }
+
+}
